98c07059e427706c9ef38217ce4b8f900115d363
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-07-26 22:54:42 -0700

    commit tree 41a2182918af5bce9e52bf9188dc00c478169cbc

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,9 @@
-// write your CatList component here
+import React from "react";
+
+function CatList ({catPics}) {
+    return (
+        catPics.map((pic) => <img alt="cat" src={pic.url}></img>)
+    )
+}
+
+export default CatList
